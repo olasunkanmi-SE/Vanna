940b60322b2593d450c8478e7dea451bca3fc24e
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-08-10 08:01:35 +0800

    create the swipe states

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,20 @@
+import { FakeBackendInterceptor } from './interceptors/fake-backend-interceptor';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './modules/material/material.module';
 import { throwIfAlreadyLoaded } from './guards/module-import.guard';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, MaterialModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: FakeBackendInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
--- src/app/modules/videos/videos-routing.module.ts
+++ src/app/modules/videos/videos-routing.module.ts
@@ -3,10 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { VideosComponent } from './videos.component';
 
-const routes: Routes = [{ path: '', component: VideosComponent }];
+import { VideoPageComponent } from './video/container/video-page/video-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: VideosComponent,
+    children: [{ path: 'video', component: VideoPageComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class VideosRoutingModule { }
+export class VideosRoutingModule {}
