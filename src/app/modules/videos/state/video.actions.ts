706644ac6af6ad28eb864c40e566666439313bef
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-08-10 14:43:11 +0800

    create the videos actions

---
+++ src/app/modules/videos/state/video.actions.ts
@@ -0,0 +1,24 @@
+import { Video } from './../model/video';
+import { Action } from '@ngrx/store';
+
+export enum VideoActionTypes {
+  LOAD_VIDEOS = '[VIdeo] Load Videos',
+  LOAD_VIDEOS_SUCCESS = '[Video] Load Videos Success',
+  LOAD_VIDEOS_FAILURE = '[Video] Load Videos Failure',
+}
+
+export class LoadVideos implements Action {
+  readonly type = VideoActionTypes.LOAD_VIDEOS;
+}
+
+export class LoadVideosSuccess implements Action {
+  readonly type = VideoActionTypes.LOAD_VIDEOS_SUCCESS;
+  constructor(public payload: Video[]) {}
+}
+
+export class LoadVideosFailure implements Action {
+  readonly type = VideoActionTypes.LOAD_VIDEOS_FAILURE;
+  constructor(public payload: string) {}
+}
+
+export type VideoActions = LoadVideos | LoadVideosSuccess | LoadVideosFailure;
