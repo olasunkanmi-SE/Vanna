b31f8d448ee427aac33b39ca28af3b0a16ebfa95
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-08-12 23:37:12 +0800

    create actions, reducer and effect from a single video

--- src/app/modules/videos/state/video.effects.ts
+++ src/app/modules/videos/state/video.effects.ts
@@ -1,9 +1,10 @@
+import { Action } from '@ngrx/store';
 import { Video } from './../model/video';
 import { mergeMap, map, catchError, exhaustMap } from 'rxjs/operators';
 import * as videoActions from './video.actions';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 import { VideoService } from './../service/video.service';
-import { Actions, Effect, ofType, createEffect } from '@ngrx/effects';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -11,18 +12,30 @@ export class VideoEffect {
   constructor(private actions$: Actions, private videoService: VideoService) {}
 
   /**
-   * Use create effects to load simulate the video actions
+   * Create effects to load simulate the video actions
    * @return the videos observable or return an error
    */
-  @Effect()
-  LoadVideos$ = createEffect(() =>
+
+  LoadVideos$: Observable<Action> = createEffect(() =>
     this.actions$.pipe(
       ofType<videoActions.LoadVideos>(
         videoActions.VideoActionTypes.LOAD_VIDEOS
       ),
-      exhaustMap((actions: videoActions.LoadVideos) =>
+      exhaustMap((action: videoActions.LoadVideos) =>
         this.videoService.getVideos().pipe(
           map((videos: Video[]) => new videoActions.LoadVideosSuccess(videos)),
+          catchError((err) => of(new videoActions.LoadVideosFailure(err)))
+        )
+      )
+    )
+  );
+
+  LoadVideo$: Observable<Action> = createEffect(() =>
+    this.actions$.pipe(
+      ofType<videoActions.LoadVideo>(videoActions.VideoActionTypes.LOAD_VIDEO),
+      exhaustMap((action: videoActions.LoadVideo) =>
+        this.videoService.getVideoById(action.payload).pipe(
+          map((video: Video) => new videoActions.LoadVideoSuccess(video)),
           catchError((err) => of(new videoActions.LoadVideosFailure(err)))
         )
       )
