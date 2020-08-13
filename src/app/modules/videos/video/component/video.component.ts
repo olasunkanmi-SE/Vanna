7ea2111a3a08090433885f36d5a2cfec50522379
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-08-14 07:19:02 +0800

    add videos in stack of cards

--- src/app/modules/videos/video/component/video.component.ts
+++ src/app/modules/videos/video/component/video.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Video } from './../../model/video';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-video',
@@ -6,9 +7,20 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./video.component.scss'],
 })
 export class VideoComponent implements OnInit {
-  @Input() video;
+  @Input() video: Video;
+  @ViewChild('videoPlayer', { static: false })
+  videoplayer: ElementRef;
+  isPlay: boolean = false;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  toggleVideo(event?: any) {
+    if (this.isPlay) {
+      this.videoplayer.nativeElement.play();
+    } else {
+      !this.isPlay;
+    }
+  }
 }
